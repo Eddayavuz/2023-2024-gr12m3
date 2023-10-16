a2d04f31a86a923ff156746f4c225f842ef85243
Eda <edda.yavuz@gmail.com>
2023-10-16 22:40:37 +0300

    Update SQL_DB_TABLE_CREAT.sql

--- SQL Queries/SQL_DB_TABLE_CREAT.sql
+++ SQL Queries/SQL_DB_TABLE_CREAT.sql
@@ -34,3 +34,40 @@ CREATE TABLE Payment_Method (
     Customer_ID INT,
     FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
 );
+
+-- Insert values into the Customer table
+
+INSERT INTO Customer (Customer_ID, Name, Email_Address)
+VALUES
+    (1, 'Eda Yavuz', 'e.yavuz@acsbg.org'),
+    (2, 'Jane Smith', 'jane.smith@email.com'),
+    (3, 'Bob Johnson', 'bob.johnson@email.com');
+
+-- Insert values into the Product table
+INSERT INTO Product (Product_ID, Name, Description, Price)
+VALUES
+    (101, 'Water Bottle', 'Description for Product A', 39.99),
+    (102, 'TV Remote', 'Description for Product B', 29.99),
+    (103, 'Socks', 'Description for Product C', 9.99);
+
+-- Insert values into the Order table
+INSERT INTO Order (Order_ID, Customer_ID)
+VALUES
+    (1001, 1),
+    (1002, 2),
+    (1003, 3);
+
+-- Insert values into the Order_Product table
+INSERT INTO Order_Product (Order_ID, Product_ID)
+VALUES
+    (1001, 101),
+    (1001, 102),
+    (1002, 102),
+    (1003, 103);
+
+-- Insert values into the Payment_Method table
+INSERT INTO Payment_Method (Payment_Method_ID, Customer_ID)
+VALUES
+    (201, 1),
+    (202, 2),
+    (203, 3);
