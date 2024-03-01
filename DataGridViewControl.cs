e114f3de0c7c905e3131a7b4415bd3c39df58cb4
Eda <edda.yavuz@gmail.com>
2024-03-01 13:40:14 +0200

    Update DataGridViewControl.cs
    fix:  if a filter wasn't selected, don't include it in the sql query

--- DataGridViewControl.cs
+++ DataGridViewControl.cs
@@ -13,17 +13,6 @@ namespace LavandulaHotel
             InitializeComponent();
         }
 
-
-        // Event handler for when the form is loaded
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Set default values for ComboBoxes when the form is loaded
-            roomType.SelectedIndex = 0;
-            availability.SelectedIndex = 0;
-            status.SelectedIndex = 0;
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             using (SqlConnection sqlCon = new SqlConnection(@"Data Source=LAB109PC23\SQLEXPRESS; Initial Catalog=lavandulaDB; Integrated Security=True;"))
@@ -32,50 +21,57 @@ namespace LavandulaHotel
 
                 // Start with a base SQL query
                 string query = "SELECT * FROM rooms WHERE 1 = 1";
+                    // Create a list to store the conditions for filtering
+                    List<string> conditions = new List<string>();
 
-                // Create a list to store the conditions for filtering
-                List<string> conditions = new List<string>();
+                    // Check each ComboBox and add a condition if an item is selected
+                    if (status.SelectedItem != null)
+                    {
+                        conditions.Add("cleaningStatus = @status");
+                    }
 
-                // Check each ComboBox and add a condition if an item is selected
-                if (status.SelectedItem != null)
-                    conditions.Add("cleaningStatus = @status");
+                    if (roomType.SelectedItem != null)
+                    {
+                        conditions.Add("roomType = @roomType");
+                    }
 
-                if (roomType.SelectedItem != null)
-                    conditions.Add("roomType = @roomType");
+                    if (availability.SelectedItem != null)
+                    {
+                        conditions.Add("roomAvailability = @roomAvailability");
+                    }
 
-                if (availability.SelectedItem != null)
-                    conditions.Add("roomAvailability = @roomAvailability");
+                    // Combine the conditions into the SQL query
+                    if (conditions.Count > 0)
+                    {
+                        query += " AND " + string.Join(" AND ", conditions);
+                    }
 
-                // combine the conditions into the SQL query
-                if (conditions.Count > 0)
-                {
-                    query += " AND " + string.Join(" AND ", conditions);
-                }
+                    SqlCommand cmd = new SqlCommand(query, sqlCon);
 
-                SqlCommand cmd = new SqlCommand(query, sqlCon);
+                    // Set parameters based on selected values, handling the case where ComboBox is empty
+                    if (status.SelectedItem != null)
+                    {
+                        cmd.Parameters.AddWithValue("@status", status.SelectedItem.ToString());
+                    }
 
-                // Set parameters based on selected values
-                cmd.Parameters.AddWithValue("@status", status.SelectedItem.ToString());
-                cmd.Parameters.AddWithValue("@roomType", roomType.SelectedItem.ToString());
-                cmd.Parameters.AddWithValue("@roomAvailability", availability.SelectedItem.ToString());
+                    if (roomType.SelectedItem != null)
+                    {
+                        cmd.Parameters.AddWithValue("@roomType", roomType.SelectedItem.ToString());
+                    }
 
-                // Use SqlDataAdapter to fetch data and populate DataGridView
-                using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
-                {
-                    DataSet ds = new DataSet();
-                    adapter.Fill(ds);
-                    dataGridView1.DataSource = ds.Tables.Count > 0 ? ds.Tables[0] : null;
+                    if (availability.SelectedItem != null)
+                    {
+                        cmd.Parameters.AddWithValue("@roomAvailability", availability.SelectedItem.ToString());
+                    }
+
+                    // Use SqlDataAdapter to fetch data and populate DataGridView
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                        dataGridView1.DataSource = ds.Tables.Count > 0 ? ds.Tables[0] : null;
+                    }
                 }
             }
-        }
-
-        private void clear_Click(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = null;
-            roomType.SelectedIndex = 0;
-            availability.SelectedIndex = 0;
-            status.SelectedIndex = 0;
-        }
-
     }
 }
