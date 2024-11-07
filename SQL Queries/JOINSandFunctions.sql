b674d622cb59bdf367d08697ebbfc3a7d5e1f7fa
Eda <edda.yavuz@gmail.com>
2024-11-07 10:22:47 +0200

    Update JOINSandFunctions.sql

--- SQL Queries/JOINSandFunctions.sql
+++ SQL Queries/JOINSandFunctions.sql
@@ -1,20 +1,3 @@
--- ALTER THE TABLES TO ADD FOREIGN KEY CONSTRAINTS
-ALTER TABLE Orders
-ADD FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
-
---INSERT, UPDATE and DELETE Examples
-
-INSERT INTO Shippers(ShipperID, ShipperName, Phone)
-VALUES ('120', 'Speedy', '0700 17 001')
-
-
-UPDATE Customers
-SET Address = 'New address here'
-WHERE CustomerName='Lonesome Pine Restaurant'
-
-DELETE FROM Orders
-WHERE CustomerID = '81'
-
 -- A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
 -- We know that Orders and Customers tables are related.
 -- The relationship between the two tables is the "CustomerID" column.
@@ -23,8 +6,7 @@ WHERE CustomerID = '81'
 -- that selects records that have matching values in both tables:
 
 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate, Customers.Country
-FROM Orders
-INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 
 -- DIFFERENT TYPES OF SQL JOINs 
 
@@ -36,19 +18,14 @@ INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 -- INNER JOIN: 
 -- Retrieve the names of customers with the count of orders they have placed (only for the matching value)
 SELECT Customers.CustomerName, COUNT(Orders.OrderID) AS OrderCount
-FROM Customers
-INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
 GROUP BY Customers.CustomerName
-
--- Can someone check how many customers do we have in total?
 
 -- LEFT JOIN: 
 -- Retrieve the names of all the customers from Customers(left table) 
--- and the order count from the Orders (right table). Display 0 or null when the customer without any orders.
-
+-- and the order count from the Orders (right table). Display 0 or null when the customer is without any orders.
 SELECT Customers.CustomerName, COUNT(Orders.OrderID) AS OrderCount
-FROM Customers
-LEFT JOIN Orders ON  Customers.CustomerID = Orders.CustomerID 
+FROM Orders RIGHT JOIN Customers ON  Customers.CustomerID = Orders.CustomerID 
 GROUP BY Customers.CustomerName
 
 -- RIGHT JOIN:
@@ -57,16 +34,12 @@ GROUP BY Customers.CustomerName
 -- If there is a shipper that is not assigned to an order, still show the shipper details.
 
 SELECT Shippers.ShipperName, Shippers.Phone, Orders.OrderID, Orders.OrderDate
-From Orders
-RIGHT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
-
-
+From Orders RIGHT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
 
 -- CREATE A NEW TABLE USING TWO DIFFERENT TABLES
 
 SELECT 
-Shippers.ShipperName,
-Orders.OrderID
+Shippers.ShipperName, Orders.OrderID
 INTO DeliverySummary
 FROM Shippers
 INNER JOIN Orders ON Shippers.ShipperID = Orders.ShipperID
